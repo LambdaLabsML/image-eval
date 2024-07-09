4393aeb42124176054ae77f22e9c7552e2d0e83f
eolecvk <eolecvk@gmail.com>
2024-07-08 23:54:04 -0700

    checkbox + refactoring repo structure

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 import streamlit as st
-from diffusers import StableDiffusionPipeline, StableDiffusion3Pipeline, AutoPipelineForText2Image
 import torch
-from PIL import ImageOps
-import requests
 from huggingface_hub import HfApi, HfFolder
+from utils.utils import get_device, generate_image
 
 # Set environment variable to turn off oneDNN custom operations
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
@@ -19,88 +17,18 @@ if HF_TOKEN is None:
 HfFolder.save_token(HF_TOKEN)
 api = HfApi()
 
-
-# Function to fetch and filter top trendy text-to-image models
-def get_top_trendy_text_to_image_models():
-
-    # Deprecating search of trendy because too many issues (eg: exclude=['stability', 'inpainting'])
-    # models = huggingface_hub.list_models(filter="text-to-image", sort="downloads", direction=-1, limit=10)
-    # models = [m.id for m in models]
-    # models = [model_id for model_id in models if not any(exclude_word in model_id for exclude_word in exclude)]
-
-    models = [
-        "stabilityai/stable-diffusion-3-medium-diffusers",
-        # "stabilityai/sdxl-turbo", OOM error
-        #"stabilityai/stable-diffusion-xl-base-1.0",
-        #"Kwai-Kolors/Kolors",
-        #"mann-e/Mann-E_Dreams", 
-        "runwayml/stable-diffusion-v1-5",
-        "stabilityai/stable-diffusion-2-1",
-        #"cagliostrolab/animagine-xl-3.1",
-        #"ByteDance/SDXL-Lightning",
-        #"ByteDance/Hyper-SD",
-    ]
-    return models
-
-# Fetch the top trendy models
-models = get_top_trendy_text_to_image_models()
-
-if models:
-    # Create dropdowns for selecting models
-    model_1 = st.selectbox("Select the first text-to-image model", models)
-    model_2 = st.selectbox("Select the second text-to-image model", models)
-else:
-    st.error("No models available to select.")
-
 # List of diverse prompts
-prompts = [
-    "A futuristic cityscape at sunset with flying cars",
-    "A serene beach with crystal clear water and palm trees",
-    "A bustling marketplace in an ancient city",
-    "A snowy mountain landscape with a cozy cabin",
-    "A vibrant underwater scene with colorful coral reefs",
-    "A fantasy castle on a floating island",
-    "A dense jungle with exotic wildlife",
-    "A futuristic robot in a high-tech laboratory",
-    "A peaceful countryside with rolling hills and farmhouses",
-    "A mysterious cave with glowing crystals",
-    "A crowded city street at night with neon lights",
-    "A space station orbiting a distant planet"
-]
+from utils.prompts import get_prompts
+prompts = get_prompts()
+prompts = prompts[:2]
 
-def get_device(index):
-    return torch.device(f'cuda:{index}')
+# List of text-to-image models
+# from utils.models import get_models
+# models = get_models()
+models_debug = ["stabilityai/stable-diffusion-2-1"]
+model_1 = st.selectbox("Select the first text-to-image model", models_debug)
+model_2 = st.selectbox("Select the second text-to-image model", models_debug)
 
-@st.cache_resource
-def load_model(model_name):
-
-    print(f"[ ] Loading model {model_name}")
-    if model_name in ["stabilityai/stable-diffusion-3-medium-diffusers"]:
-        pipe = StableDiffusion3Pipeline.from_pretrained(
-            model_name,
-            torch_dtype=torch.float16,
-            low_cpu_mem_usage=True
-            )
-        
-    elif model_name in ["stabilityai/sdxl-turbo"]:
-        pipe = AutoPipelineForText2Image.from_pretrained(
-            model_name,
-            torch_dtype=torch.float16,
-            variant="fp16")
-    else:
-        pipe = StableDiffusionPipeline.from_pretrained(
-            model_name, 
-            torch_dtype=torch.float16,
-            low_cpu_mem_usage=True  # Enable low CPU memory usage
-    )
-    print(f"[+] Loaded model {model_name}")
-    return pipe
-
-def generate_image(model_name, prompt, device):
-    pipe = load_model(model_name)
-    pipe = pipe.to(device)  # Use specified GPU for inference
-    image = pipe(prompt).images[0]
-    return image
 
 # CSS to center the prompts vertically
 st.markdown(
@@ -118,21 +46,31 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+# Initialize session state for storing generated images
+if 'generated_images' not in st.session_state:
+    st.session_state.generated_images = {}
+
 if st.button("Generate"):
     num_gpus = torch.cuda.device_count()
     gpu_index = 0
 
     for prompt in prompts:
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.markdown(f"<div class='center-vertical'>{prompt}</div>", unsafe_allow_html=True)
-        with col2:
-            device = get_device(gpu_index % num_gpus)
-            image_1 = generate_image(model_1, prompt, device)
-            st.image(image_1)
-            gpu_index += 1
-        with col3:
-            device = get_device(gpu_index % num_gpus)
-            image_2 = generate_image(model_2, prompt, device)
-            st.image(image_2)
-            gpu_index += 1
+        st.session_state.generated_images[prompt] = {}
+        device = get_device(gpu_index % num_gpus)
+        image_1 = generate_image(model_1, prompt, device)
+        image_2 = generate_image(model_2, prompt, device)
+        st.session_state.generated_images[prompt]['image_1'] = image_1
+        st.session_state.generated_images[prompt]['image_2'] = image_2
+        gpu_index += 2
+
+# Display stored images with checkboxes
+for prompt, images in st.session_state.generated_images.items():
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown(f"<div class='center-vertical'>{prompt}</div>", unsafe_allow_html=True)
+    with col2:
+        st.image(images['image_1'])
+        st.checkbox("Select Image 1", key=f"checkbox_{prompt}_1_persist")
+    with col3:
+        st.image(images['image_2'])
+        st.checkbox("Select Image 2", key=f"checkbox_{prompt}_2_persist")
---
+++ utils/prompts.py
@@ -0,0 +1,15 @@
+def get_prompts():
+    return [
+    "A futuristic cityscape at sunset with flying cars",
+    "A serene beach with crystal clear water and palm trees",
+    "A bustling marketplace in an ancient city",
+    "A snowy mountain landscape with a cozy cabin",
+    "A vibrant underwater scene with colorful coral reefs",
+    "A fantasy castle on a floating island",
+    "A dense jungle with exotic wildlife",
+    "A futuristic robot in a high-tech laboratory",
+    "A peaceful countryside with rolling hills and farmhouses",
+    "A mysterious cave with glowing crystals",
+    "A crowded city street at night with neon lights",
+    "A space station orbiting a distant planet"
+]
