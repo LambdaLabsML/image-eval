e40cc16e95ca972040eafdbe2de9fd5f96376cf3
eolecvk <eolecvk@gmail.com>
2024-07-14 16:06:50 +0700

    typo in api_server script

--- models/opensora/api_server.py
+++ models/opensora/api_server.py
@@ -39,7 +39,7 @@ def generate_image():
         available_gpus = get_available_gpus()
 
         # forcing single GPU until bug resolved https://github.com/orgs/LambdaLabsML/projects/14/views/1?pane=issue&itemId=70850852
-        available_gpu = available_gpu[0] 
+        available_gpu = available_gpus[0] 
         if not available_gpus:
             return jsonify({'message': 'No available GPUs found'}), 500
 
