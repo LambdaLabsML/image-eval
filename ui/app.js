871b0a37d7404520020bb576470817235ad4cfd7
eolecvk <eolecvk@gmail.com>
2024-07-16 20:48:30 +0700

    ui relative paths

--- ui/app.js
+++ ui/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/home/eole/Workspaces/image-eval/prompts/all_category.txt')
+    fetch('prompts.txt')
         .then(response => response.text())
         .then(data => {
             const prompts = data.split('\n').filter(prompt => prompt.trim() !== '');
@@ -15,14 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 const modelACell = document.createElement('td');
                 const modelAVideo = document.createElement('video');
                 modelAVideo.controls = true;
-                modelAVideo.src = `/home/eole/Desktop/sora1.2/sample_${index.toString().padStart(4, '0')}.mp4`;
+                modelAVideo.src = `sora1.2/sample_${index.toString().padStart(4, '0')}.mp4`;
                 modelACell.appendChild(modelAVideo);
                 row.appendChild(modelACell);
 
                 const modelBCell = document.createElement('td');
                 const modelBVideo = document.createElement('video');
                 modelBVideo.controls = true;
-                modelBVideo.src = `/home/eole/Desktop/sora1.2/sample_${index.toString().padStart(4, '0')}.mp4`;
+                modelBVideo.src = `sora1.2/sample_${index.toString().padStart(4, '0')}.mp4`;
                 modelBCell.appendChild(modelBVideo);
                 row.appendChild(modelBCell);
 
